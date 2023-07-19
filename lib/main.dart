b8e5a601ba9789ecb87586764572410759c66991
unknown <joao_marcos2002@hotmail.com>
2023-07-18 21:59:53 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:test/data/task_inherited.dart';
+import 'package:test/screens/tela_inicial.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TaskInherited(child: const InitialScreen()),
+    );
+  }
+}
